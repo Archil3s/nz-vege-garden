a987808d3ef0d53fb0fda62e104c22649f0091a7
Archil3s <82018448+Archil3s@users.noreply.github.com>
2026-05-05 10:58:09 +1200

    Add app shell and navigation

---
+++ lib/app/app.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../features/crops/crop_guide_screen.dart';
+import '../features/garden_beds/garden_beds_screen.dart';
+import '../features/home/home_screen.dart';
+import '../features/pests/pest_guide_screen.dart';
+import '../features/settings/settings_screen.dart';
+import '../features/tasks/weekly_tasks_screen.dart';
+import 'app_theme.dart';
+
+class NzVegeGardenApp extends StatelessWidget {
+  const NzVegeGardenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NZ Vege Garden',
+      theme: buildAppTheme(),
+      home: const AppShell(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _selectedIndex = 0;
+
+  static const _screens = <Widget>[
+    HomeScreen(),
+    CropGuideScreen(),
+    GardenBedsScreen(),
+    WeeklyTasksScreen(),
+    PestGuideScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() => _selectedIndex = index);
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.menu_book_outlined),
+            selectedIcon: Icon(Icons.menu_book),
+            label: 'Crops',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.yard_outlined),
+            selectedIcon: Icon(Icons.yard),
+            label: 'Beds',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.checklist_outlined),
+            selectedIcon: Icon(Icons.checklist),
+            label: 'Tasks',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bug_report_outlined),
+            selectedIcon: Icon(Icons.bug_report),
+            label: 'Pests',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}

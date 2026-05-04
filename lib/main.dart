504a31227a9cd7c15e630f738046d00cbd03cddf
Archil3s <82018448+Archil3s@users.noreply.github.com>
2026-05-05 11:46:46 +1200

    feat(notifications): initialise local notifications on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'services/notifications/local_notification_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalNotificationService.instance.initialise();
+
   runApp(const NzVegeGardenApp());
 }
